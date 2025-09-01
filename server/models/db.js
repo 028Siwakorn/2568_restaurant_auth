701cf3b520ac02b8b41ccf04197d546c0dfb1f70
Siwakorn <664259028@webmail.npru.ac.th>
2025-09-01 14:25:59 +0700

    Depoly1

--- server/models/db.js
+++ server/models/db.js
@@ -1,11 +1,16 @@
 import { Sequelize } from "sequelize";
 import dbConfig from "../config/db.config.js";
-
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   logging: false,
+  dialectOptions: {
+    ssl: {
+      required: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const testConnection = async () => {
